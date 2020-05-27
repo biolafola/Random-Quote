843fbb172368e21c0c72d31b2340022d9d38d0f9
unknown <biolafolaranmi@outlook.com>
2020-05-27 12:27:53 +0100

    Updates

---
+++ precache-manifest.cfaf079512226095a093280821abdcda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a3f6a4a2ff7f44125907f8359a805e",
+    "url": "/Random-Quote/index.html"
+  },
+  {
+    "revision": "8e38d40db43f08ccd6c1",
+    "url": "/Random-Quote/static/css/main.0007fa1c.chunk.css"
+  },
+  {
+    "revision": "ce7c83e346e5fbe102d3",
+    "url": "/Random-Quote/static/js/2.aac49012.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote/static/js/2.aac49012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e38d40db43f08ccd6c1",
+    "url": "/Random-Quote/static/js/main.d34acf64.chunk.js"
+  },
+  {
+    "revision": "8b539e53fdf022454851",
+    "url": "/Random-Quote/static/js/runtime-main.35f40c4b.js"
+  }
+]);
